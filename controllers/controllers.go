8899c25550a0274d28cddd8de3ba49dcac994c2a
Jacob Kim <jpoly1219@gmail.com>
2021-06-01 13:52:18 +0000

    Edited controllers.go's imports to use models.go
    - The controllers require access to data in order to manipulate them. Since the data is inside models.Post, I updated Posts to models.Posts.

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -5,28 +5,21 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/jpoly1219/go-blog/models"
+
 	"github.com/gorilla/mux"
 )
 
-type Post struct {
-	Id      string `json:"Id"`
-	Title   string `json:"Title"`
-	Author  string `json:"Author"`
-	Content string `json:"Content"`
-}
-
-var Posts = make([]Post, 0)
-
 func ReturnAllPosts(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("returning all posts...")
-	json.NewEncoder(w).Encode(Posts)
+	json.NewEncoder(w).Encode(models.Posts)
 }
 
 func ReturnSinglePost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	keys := vars["id"]
 
-	for _, post := range Posts {
+	for _, post := range models.Posts {
 		if post.Id == keys {
 			json.NewEncoder(w).Encode(post)
 		}
@@ -34,23 +27,23 @@ func ReturnSinglePost(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateNewPost(w http.ResponseWriter, r *http.Request) {
-	var newPost Post
+	var newPost models.Post
 	json.NewDecoder(r.Body).Decode(&newPost)
-	Posts = append(Posts, newPost)
+	models.Posts = append(models.Posts, newPost)
 	json.NewEncoder(w).Encode(newPost)
-	fmt.Println(Posts)
+	fmt.Println(models.Posts)
 }
 
 func DeletePost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	keys := vars["id"]
 
-	for i, post := range Posts {
+	for i, post := range models.Posts {
 		if post.Id == keys {
 			fmt.Println(keys)
-			Posts = append(Posts[:(i)], Posts[i+1:]...)
+			models.Posts = append(models.Posts[:(i)], models.Posts[i+1:]...)
 			json.NewEncoder(w).Encode(post)
 		}
 	}
-	fmt.Println(Posts)
+	fmt.Println(models.Posts)
 }
