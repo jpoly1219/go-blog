c76b0d19374a7a099824772af95b4548863b6935
Jacob Kim <jpoly1219@gmail.com>
2021-06-01 14:10:31 +0000

    Now declares instances of models.Post
    - Controllers will delcare instances of models.Post according to their needs.

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -10,16 +10,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
+var Posts = make([]models.Post, 0)
+
 func ReturnAllPosts(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("returning all posts...")
-	json.NewEncoder(w).Encode(models.Posts)
+	json.NewEncoder(w).Encode(Posts)
 }
 
 func ReturnSinglePost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	keys := vars["id"]
 
-	for _, post := range models.Posts {
+	for _, post := range Posts {
 		if post.Id == keys {
 			json.NewEncoder(w).Encode(post)
 		}
@@ -29,21 +31,21 @@ func ReturnSinglePost(w http.ResponseWriter, r *http.Request) {
 func CreateNewPost(w http.ResponseWriter, r *http.Request) {
 	var newPost models.Post
 	json.NewDecoder(r.Body).Decode(&newPost)
-	models.Posts = append(models.Posts, newPost)
+	Posts = append(Posts, newPost)
 	json.NewEncoder(w).Encode(newPost)
-	fmt.Println(models.Posts)
+	fmt.Println(Posts)
 }
 
 func DeletePost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	keys := vars["id"]
 
-	for i, post := range models.Posts {
+	for i, post := range Posts {
 		if post.Id == keys {
 			fmt.Println(keys)
-			models.Posts = append(models.Posts[:(i)], models.Posts[i+1:]...)
+			Posts = append(Posts[:(i)], Posts[i+1:]...)
 			json.NewEncoder(w).Encode(post)
 		}
 	}
-	fmt.Println(models.Posts)
+	fmt.Println(Posts)
 }
