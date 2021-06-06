d22fa0d85b4b6d483dbea9ba16c8c425b71792fd
Jacob Kim <jpoly1219@gmail.com>
2021-06-06 08:01:49 +0000

    Signup and Login functionality implemented
    - SignUp() accepts POST request with new user data, and inserts it into the "users" database.
    
    - LogIn() accepts POST request with existing user data, and compares it with the entry in the "users" database.
    
    - LogIn() needs to be able to give authenticated users JWT, which is the next feature to work on.

--- auth/auth.go
+++ auth/auth.go
@@ -9,15 +9,37 @@ import (
 )
 
 func SignUp(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
 
+	queryStr := fmt.Sprintf(
+		"INSERT INTO users(name, email, username, password) VALUES('%s', '%s', '%s', '%s')",
+		user.Name, user.Email, user.Username, user.Password,
+	)
+
+	results, err := models.Db.Query(queryStr)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for results.Next() {
+		err = results.Scan(&user.Name, &user.Email, &user.Username, &user.Password)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+
+	json.NewEncoder(w).Encode(user)
 }
 
 func LogIn(w http.ResponseWriter, r *http.Request) {
 	var user models.User
 	json.NewDecoder(r.Body).Decode(&user)
 
+	fmt.Println(user.Id)
+
 	queryStr := fmt.Sprintf(
-		"SELECT * FROM users WHERE email = %s AND password = %s",
+		"SELECT * FROM users WHERE email = '%s' AND password = '%s'",
 		user.Email, user.Password,
 	)
 
@@ -25,6 +47,23 @@ func LogIn(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err.Error())
 	}
-	fmt.Println(results)
+
+	type queryOutput struct {
+		id, name, email, username, password string
+	}
+
+	var qo queryOutput
+
+	for results.Next() {
+		err = results.Scan(&qo.id, &qo.name, &qo.email, &qo.username, &qo.password)
+		if err != nil {
+			panic(err.Error())
+		}
+	}
+	if qo.email == user.Email && qo.password == user.Password {
+		fmt.Println("Match!")
+	} else {
+		fmt.Println("No Match!")
+	}
 
 }
