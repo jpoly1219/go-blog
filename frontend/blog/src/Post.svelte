59012f5d838b57ea3305e3827c604ccdbd6ce5b2
Jacob Kim <jpoly1219@gmail.com>
2021-06-12 14:27:52 +0000

    Created a Post component to render each blog posts

---
+++ frontend/blog/src/Post.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let post;
+</script>
+
+<div>
+    <h3>{post.title}</h3>
+    <h4>{post.author}</h4>
+    <p>{post.content}</p>
+</div>
+
+<style>
+    div {
+        border: solid 2px grey;
+        border-radius: 30px;
+        margin: 1em;
+        padding: 10px;
+		grid-column: 3 / 4;
+    }
+</style>
