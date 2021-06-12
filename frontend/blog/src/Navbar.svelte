6295a05f6594a526d51f5ec2a4ee30563e8346c9
Jacob Kim <jpoly1219@gmail.com>
2021-06-12 14:28:18 +0000

    Created a Navbar component to have login/sign up buttons

---
+++ frontend/blog/src/Navbar.svelte
@@ -0,0 +1,24 @@
+<script></script>
+
+<div>
+    <nav>
+        <ul>
+            <li>Log In</li>
+            <li>Sign up</li>
+        </ul>
+    </nav>
+</div>
+
+<style>
+    div {
+        width: 100%;
+        z-index: 999;
+        background-color: indianred;
+        grid-column: 1 / 6;
+        justify-self: stretch;
+        text-align: center;
+    }
+    ul {
+        grid-auto-flow: column;
+    }
+</style>
