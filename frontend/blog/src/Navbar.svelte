f6506cb8f207d7732cbae1341770f3932a21eca8
Jacob Kim <jpoly1219@gmail.com>
2021-06-13 05:24:06 +0000

    Implemented styling to navigation bar

--- frontend/blog/src/Navbar.svelte
+++ frontend/blog/src/Navbar.svelte
@@ -1,14 +1,17 @@
 <script></script>
 
-<div>
-    <nav>
-        <ul>
-            <li>Log In</li>
-            <li>Sign up</li>
-        </ul>
-    </nav>
-</div>
-
-<style>
-    
-</style>
+<header class="sticky top-0 bg-white text-gray-600 body-font z-10">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a href="#" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span class="ml-3 text-xl">Goblog</span>
+        </a>
+        <nav class="md:ml-auto flex flex-wrap item-center text-base justify-center">
+            <button class="mr-5 inline-flex items-center border border-gray-500 rounded-lg">
+                <span class="mx-3 my-2">Log in</span>
+            </button>
+            <button class="inline-flex items-center bg-blue-400 border border-blue-900 rounded-lg text-base">
+                <span class="mx-3 my-2 text-white">Sign up</span>
+            </button>
+        </nav>
+    </div>
+</header>
