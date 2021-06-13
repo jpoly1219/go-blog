d5ed23a66652926b9371cce3ae9f9c40272355eb
Jacob Kim <jpoly1219@gmail.com>
2021-06-13 05:24:26 +0000

    Implemented styling to individual posts

--- frontend/blog/src/Post.svelte
+++ frontend/blog/src/Post.svelte
@@ -1,13 +1,16 @@
 <script>
     export let post;
 </script>
-
-<div>
-    <h3>{post.title}</h3>
-    <h4>{post.author}</h4>
-    <p>{post.content}</p>
+<div class="container mx-auto px-96 py-10 flex flex-col">
+    <div class="border-b-2">
+        <h2 class="text-2xl font-medium text-gray-900 mt-4 mb-4">
+            {post.title}
+        </h2>
+        <h3 class="font-medium text-gray-900">
+            {post.author}
+        </h3>
+        <p class="leading-relaxed mb-8">
+            {post.content}
+        </p>
+    </div>
 </div>
-
-<style>
-    
-</style>
