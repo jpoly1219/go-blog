4265704b2bdc9aeaae21ff0e29d0af52f8fc3055
Jacob Kim <jpoly1219@gmail.com>
2021-06-13 14:49:11 +0000

    Created Home component to hold posts
    - Decided to treat Navbar component as a reusable component that each page component can use.
    
    - Instead of loading Post.svelte and Navbar.svelte in App.svelte, it is a better idea to load both of them inside Home.svelte, then load the Home.svelte inside App.svelte.
    
    - This also helps with svelte-routing, as the library is a bit dumb and cannot figure out where to route without using its <Link/> component.

---
+++ frontend/blog/src/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+    import Navbar from "./Navbar.svelte"
+    import Post from "./Post.svelte"
+    export let posts;
+</script>
+
+<Navbar/>
+{#each posts as item}
+    <Post post={item}/>
+{/each}
