4b64cf7268b959503d6a8e7510762fc20ee71cf8
Jacob Kim <jpoly1219@gmail.com>
2021-06-13 14:52:02 +0000

    Navbar component is now a reusable component
    - Navbar also imports the <Link/> component from svelte-router, and uses that for its Login and Sign up buttons.
    
    - Each page component will load Navbar for a consistent UI.

--- frontend/blog/src/Navbar.svelte
+++ frontend/blog/src/Navbar.svelte
@@ -1,16 +1,20 @@
-<script></script>
+<script>
+    import { Link } from "svelte-routing";
+</script>
 
 <header class="sticky top-0 bg-white text-gray-600 body-font z-10">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <a href="#" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <Link to="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <span class="ml-3 text-xl">Goblog</span>
-        </a>
+        </Link>
         <nav class="md:ml-auto flex flex-wrap item-center text-base justify-center">
-            <button class="mr-5 inline-flex items-center border border-gray-500 rounded-lg">
-                <span class="mx-3 my-2">Log in</span>
-            </button>
+            <Link to="/login">
+                <button class="mr-5 inline-flex items-center border border-gray-500 rounded-lg">
+                    <span class="mx-3 my-2">Log in</span>
+                </button>
+            </Link>
             <button class="inline-flex items-center bg-blue-400 border border-blue-900 rounded-lg text-base">
-                <span class="mx-3 my-2 text-white">Sign up</span>
+                <Link to="/signup" class="mx-3 my-2 text-white">Sign up</Link>
             </button>
         </nav>
     </div>
