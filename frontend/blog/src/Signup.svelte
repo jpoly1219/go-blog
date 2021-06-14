1cc1100b3c4c6f7dc276c3fa05b14ed82ddafd5d
Jacob Kim <jpoly1219@gmail.com>
2021-06-14 14:17:35 +0000

    Created Signup component to render signup page
    - Signup also forwards the "pressed" event to App by using on:`event` tag provided by Svelte.

---
+++ frontend/blog/src/Signup.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Navbar from "./Navbar.svelte";
+</script>
+
+<Navbar on:pressed/>
+<div class="bg-white w-full rounded-lg border-gray-900">
+    <span>Signup Page</span>
+</div>
