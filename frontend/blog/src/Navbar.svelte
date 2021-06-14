b9c1c97159bfcfad000a43e0444aca08c11dc3a9
Jacob Kim <jpoly1219@gmail.com>
2021-06-14 14:18:15 +0000

    Navbar now dispatches custom event to the components that renders it
    - Using createEventDispatcher provided by Svelte, Navbar uses three dispatchers to handle different button clicks.
    
    - These events carry a string that determines what page the app should load, which is processed in App.
    
    - Edited some HTML to remove svelte-routing and use default HTML components.

--- frontend/blog/src/Navbar.svelte
+++ frontend/blog/src/Navbar.svelte
@@ -1,20 +1,30 @@
 <script>
-    import { Link } from "svelte-routing";
+    import { createEventDispatcher } from "svelte"
+
+    const dispatch = createEventDispatcher();
+
+    function homePressed() {
+        dispatch("pressed", "home");
+    }
+    function loginPressed() {
+        dispatch("pressed", "login");
+    }
+    function signupPressed() {
+        dispatch("pressed", "signup");
+    }
 </script>
 
 <header class="sticky top-0 bg-white text-gray-600 body-font z-10">
     <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <Link to="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <span class="ml-3 text-xl">Goblog</span>
-        </Link>
+        <a href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            <span on:click={homePressed} class="ml-3 text-xl">Goblog</span>
+        </a>
         <nav class="md:ml-auto flex flex-wrap item-center text-base justify-center">
-            <Link to="/login">
-                <button class="mr-5 inline-flex items-center border border-gray-500 rounded-lg">
-                    <span class="mx-3 my-2">Log in</span>
-                </button>
-            </Link>
-            <button class="inline-flex items-center bg-blue-400 border border-blue-900 rounded-lg text-base">
-                <Link to="/signup" class="mx-3 my-2 text-white">Sign up</Link>
+            <button on:click={loginPressed} class="mr-5 inline-flex items-center border border-gray-500 rounded-lg">
+                <span class="mx-3 my-2">Log in</span>
+            </button>
+            <button on:click={signupPressed} class="inline-flex items-center bg-blue-400 border border-blue-900 rounded-lg text-base">
+                <span class="mx-3 my-2 text-white">Sign up</span>
             </button>
         </nav>
     </div>
