61beb8e768f75c27552029c362fdebc9479df767
Jacob Kim <jpoly1219@gmail.com>
2021-06-16 14:56:53 +0000

    Adding a feature to send POST request with form data to main.go
    - The problem is that whenever I try to submit the form, the server blocks it by saying that the request method is now allowed. However I AM sending a POST request.
    
    - CORS is fixed by changing auth.go.
    
    - Maybe trying Axios or XHR might work better.

--- frontend/blog/src/Login.svelte
+++ frontend/blog/src/Login.svelte
@@ -1,6 +1,28 @@
 <script>
-import Navbar from "./Navbar.svelte";
+    import Navbar from "./Navbar.svelte";
 
+    let email = "";
+    let password = "";
+    let result = null;
+
+    async function checkLogin() {
+        let loginDetails = {
+            email: email,
+            password: password
+        }
+
+        const options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(loginDetails)
+        };
+        const res = await fetch("http://jpoly1219devbox.xyz:8090/auth/login", options);
+        const json = await res.json();
+        result = JSON.stringify(json);
+        alert(result);
+    }
 </script>
 
 <Navbar on:pressed/>
@@ -9,17 +31,18 @@ import Navbar from "./Navbar.svelte";
         <h2 class="text-2xl font-medium text-center mb-20">Welcome back.</h2>
         <form action="/" class="flex flex-col px-30">
             <div class="flex flex-col items-stretch px-30">
-                <input type="text" placeholder="Email address" class="border-b-2 py-4">
-                <input type="text" placeholder="Password" class="border-b-2 py-4">
+                <input bind:value={email} type="text" placeholder="Email address" class="border-b-2 py-4">
+                <input bind:value={password} type="text" placeholder="Password" class="border-b-2 py-4">
             </div>
             <div class="flex flex-col items-center my-10">
-                <button type="submit" class="border border-gray-500 rounded-lg p-3">
+                <button type="button" on:click={checkLogin} class="border border-gray-500 rounded-lg p-3">
                     <span class="mx-3 my-2">Log in</span>
                 </button>
             </div>
             <p class="text-sm font-extralight">
                 Don't have an account yet? <a href="/" class="text-blue-400">Sign up</a> today.
             </p>
+            <p>{result}</p>
         </form>
     </div>
 </div>
