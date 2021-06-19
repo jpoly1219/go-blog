809e8e098435be4517a7d1d6b48d683fa9175dd1
Jacob Kim <jpoly1219@gmail.com>
2021-06-19 08:38:03 +0000

    Fixed the backend not processing the JSON, Login now uses stores.js for later use
    - checkLogin() has been renamed to submit() and is now called in <form> instead of <button>.
    
    - JSON.stringifying the payload allows the backend to process it properly.
    
    - After receiving the JWTs from the server, Login saves the access token to stores.js.
      - If a valid access token is received, the authenticated store is set to true, and the username is extracted from the payload and sent to Navbar as a prop.

--- frontend/blog/src/Login.svelte
+++ frontend/blog/src/Login.svelte
@@ -1,11 +1,13 @@
 <script>
+    import { beforeUpdate } from "svelte"
     import Navbar from "./Navbar.svelte";
+    import { authenticated, accessToken } from "./stores.js"
 
     let email = "";
     let password = "";
     let result = null;
 
-    async function checkLogin() {
+    async function submit() {
         let loginDetails = {
             email: email,
             password: password
@@ -13,7 +15,7 @@
 
         const options = {
             method: "POST",
-            body: loginDetails,
+            body: JSON.stringify(loginDetails),
             headers: {
                 "Content-Type": "application/json"
             }
@@ -21,21 +23,30 @@
         const res = await fetch("http://jpoly1219devbox.xyz:8090/auth/login", options);
         const json = await res.json();
         result = JSON.stringify(json);
-        console.log(result);
+        accessToken.set(json.accessToken);
     }
+
+    let username = "";
+    beforeUpdate(() => {
+        if ($accessToken != undefined && $accessToken != "") {
+            authenticated.set(true);
+            username = JSON.parse(window.atob($accessToken.split(".")[1])).user_name
+        }
+    });
+
 </script>
 
-<Navbar on:pressed/>
+<Navbar on:pressed username={username}/>
 <div class="container mx-auto items-center flex flex-col text-gray-900">
     <div class="bg-white p-16 rounded-lg border-gray-900 shadow-lg my-44">
         <h2 class="text-2xl font-medium text-center mb-20">Welcome back.</h2>
-        <form action="/" class="flex flex-col px-30">
+        <form on:submit|preventDefault={submit} class="flex flex-col px-30">
             <div class="flex flex-col items-stretch px-30">
                 <input bind:value={email} type="text" placeholder="Email address" class="border-b-2 py-4">
                 <input bind:value={password} type="text" placeholder="Password" class="border-b-2 py-4">
             </div>
             <div class="flex flex-col items-center my-10">
-                <button type="button" on:click={checkLogin} class="border border-gray-500 rounded-lg p-3">
+                <button type="submit" class="border border-gray-500 rounded-lg p-3">
                     <span class="mx-3 my-2">Log in</span>
                 </button>
             </div>
