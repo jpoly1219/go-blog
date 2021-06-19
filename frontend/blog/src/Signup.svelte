f8fbbbc10ba9c3caa6be40e06c2173d849d98f58
Jacob Kim <jpoly1219@gmail.com>
2021-06-19 11:09:16 +0000

    Created signin() function, login link below now redirects to the Login component
    - The signin() function just redirects the user to the Login component for now.
      - API calls to actually create users inside the backend database will be added later.

--- frontend/blog/src/Signup.svelte
+++ frontend/blog/src/Signup.svelte
@@ -1,9 +1,12 @@
 <script>
-    import Navbar from "./Navbar.svelte";
+    import { activePage } from "./stores";
 
+    function signin() {
+        alert("now signed up!")
+        activePage.set("login")
+    }
 </script>
 
-<Navbar on:pressed/>
 <div class="container mx-auto items-center flex flex-col text-gray-900">
     <div class="bg-white p-16 rounded-lg border-gray-900 shadow-lg my-20">
         <h2 class="text-2xl font-medium text-center mb-20">Join Goblog.</h2>
@@ -15,12 +18,12 @@
                 <input type="text" placeholder="Password" class="border-b-2 py-4">
             </div>
             <div class="flex flex-col items-center my-10">
-                <button type="submit" class="border bg-blue-400 border-blue-900 rounded-lg p-3">
+                <button type="submit" on:click={signin} class="border bg-blue-400 border-blue-900 rounded-lg p-3">
                     <span class="mx-3 my-2 text-white">Sign up</span>
                 </button>
             </div>
             <p class="text-sm font-extralight">
-                Already have an account? <a href="/" class="text-blue-400">Log in</a>.
+                Already have an account? <span on:click={() => activePage.set("login")} class="text-blue-400 cursor-pointer">Log in</span>.
             </p>
         </form>
     </div>
