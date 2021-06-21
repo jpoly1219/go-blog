dacc31497527bd000cffbd7bad61e117bdfe1f5f
Jacob Kim <jpoly1219@gmail.com>
2021-06-21 11:05:46 +0000

    Username span after login redirects to a page and has proper margin
    - Username span leads to Notfound.svelte for now.

--- frontend/blog/src/Navbar.svelte
+++ frontend/blog/src/Navbar.svelte
@@ -4,20 +4,18 @@ import { beforeUpdate } from "svelte";
     import { authenticated, accessToken, activePage } from "./stores.js"
 
     function logout() {
-        authenticated.set(false);
-        accessToken.set("");
-        activePage.set("home");
+        authenticated.set(false)
+        accessToken.set("")
+        activePage.set("home")
     }
 
-    let username = "";
+    let username = ""
     beforeUpdate(() => {
         if ($authenticated == true) {
             let payloadB64 = $accessToken.split(".")[1]
             username = JSON.parse(window.atob(payloadB64)).user_name
-            console.log(JSON.parse(window.atob(payloadB64)).exp)
-            setTimeout(() => console.log(JSON.parse(window.atob(payloadB64)).exp), 2000)
         }
-    });
+    })
 </script>
 
 <header class="sticky top-0 bg-white text-gray-600 body-font z-10">
@@ -34,7 +32,9 @@ import { beforeUpdate } from "svelte";
                     <span class="mx-3 my-2 text-white">Sign up</span>
                 </button>
             {:else}
-                <span>{username}</span>
+            <div on:click={() => activePage.set("notfound")} class="flex items-center mr-5 text-base text-gray-900 cursor-pointer">
+                <span class="mx-3 my-2">{username}</span>
+            </div>
                 <button on:click={logout} class="inline-flex items-center bg-blue-400 border border-blue-900 rounded-lg text-base">
                     <span class="mx-3 my-2 text-white">Log out</span>
                 </button>
