04de30e5f943b0c0cf356e16754421d91a993aad
Jacob Kim <jpoly1219@gmail.com>
2021-06-21 11:07:31 +0000

    All stores now use export const, added expiration to hold expiration time

--- frontend/blog/src/stores.js
+++ frontend/blog/src/stores.js
@@ -1,5 +1,6 @@
 import { writable } from "svelte/store"
 
-export const authenticated = writable(false);
-export const accessToken = writable("");
-export let activePage = writable("home");
+export const authenticated = writable(false)
+export const accessToken = writable("")
+export const expiration = writable("")
+export const activePage = writable("home")
