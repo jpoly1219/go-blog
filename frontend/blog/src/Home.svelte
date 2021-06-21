fe21dd40ad637f31d9cbe51a00e08e2a84d1da36
Jacob Kim <jpoly1219@gmail.com>
2021-06-21 11:02:19 +0000

    added semicolons

--- frontend/blog/src/Home.svelte
+++ frontend/blog/src/Home.svelte
@@ -1,14 +1,14 @@
 <script>
-    import { onMount } from "svelte"
-    import Post from "./Post.svelte"
+    import { onMount } from "svelte";
+    import Post from "./Post.svelte";
     
     const apiURL = "http://jpoly1219devbox.xyz:8090/posts";
 	let data = [];
 
 	onMount(async () => {
-		const res = await fetch(apiURL);
-		data = await res.json();
-	});
+		const res = await fetch(apiURL)
+		data = await res.json()
+	})
 
 	function loadSinglePost() {
 		
