3bb579e09792e1ab7c2c917d67af2dc31171a055
Jacob Kim <jpoly1219@gmail.com>
2021-06-23 11:32:48 +0000

    Created loadSinglePost()
    - When the user clicks on the post title, it will redirect to a page dedicated for that single post.

--- frontend/blog/src/Post.svelte
+++ frontend/blog/src/Post.svelte
@@ -1,9 +1,21 @@
 <script>
+import { activePage } from "./stores";
+
+
     export let post;
+
+    function loadSinglePost() {
+        let id = post.id;
+        async () => {
+            const res = await fetch("http://jpoly1219devbox.xyz:8090/posts" + id.toString())
+            singlePost = await res.json()
+        }
+        activePage.set("singlepost")
+    }
 </script>
 <div class="container mx-auto px-96 pb-10 flex flex-col">
     <div class="border-b-2">
-        <h2 class="text-2xl font-medium text-gray-900 mt-4 mb-4">
+        <h2 on:click={loadSinglePost} class="text-2xl font-medium text-gray-900 mt-4 mb-4">
             {post.title}
         </h2>
         <h3 class="font-medium text-gray-900">
