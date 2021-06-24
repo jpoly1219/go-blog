65c65f2d49c8346d05957251c3637da5723a3baf
Jacob Kim <jpoly1219@gmail.com>
2021-06-24 13:54:42 +0000

    App is no longer responsible for loading the post that has been clicked on
    - loadSinglePost() is removed, because that is now handles in the Singlepost component.

--- frontend/blog/src/App.svelte
+++ frontend/blog/src/App.svelte
@@ -1,24 +1,18 @@
 <script>
-	import Home from "./Home.svelte";
-	import Login from "./Login.svelte";
-	import Signup from "./Signup.svelte";
-	import Notfound from "./Notfound.svelte";
-	import Navbar from "./Navbar.svelte";
-	import Singlepost from "./Singlepost.svelte";
-	import { accessToken, activePage, expiration, isSinglePost } from "./stores.js";
+	import Home from "./Home.svelte"
+	import Login from "./Login.svelte"
+	import Signup from "./Signup.svelte"
+	import Notfound from "./Notfound.svelte"
+	import Navbar from "./Navbar.svelte"
+	import Singlepost from "./Singlepost.svelte"
+	import { accessToken, activePage, expiration } from "./stores.js"
 
 	const pageMap = {
 		home: Home,
 		login: Login,
 		signup: Signup,
-		notfound: Notfound
-	};
-
-	let singlePost;
-	function loadSinglePost(event) {
-		singlePost = event.detail.post
-		console.log(singlePost)
-		isSinglePost.set(true)
+		notfound: Notfound,
+		singlepost: Singlepost
 	}
 
 	function refreshTimer() {
@@ -31,32 +25,28 @@
 						const options = {
 							method: "POST",
 							credentials: "include"
-						};
-						const res = await fetch("http://jpoly1219devbox.xyz:8090/auth/refresh", options);
-						const json = await res.json();
-						accessToken.set(json.accessToken);
-						console.log($accessToken);
-						let payloadB64 = $accessToken.split(".")[1];
-						expiration.set(JSON.parse(window.atob(payloadB64)).exp);
-					})();
-					clearInterval(timer);
+						}
+						const res = await fetch("http://jpoly1219devbox.xyz:8090/auth/refresh", options)
+						const json = await res.json()
+						accessToken.set(json.accessToken)
+						console.log($accessToken)
+						let payloadB64 = $accessToken.split(".")[1]
+						expiration.set(JSON.parse(window.atob(payloadB64)).exp)
+					})()
+					clearInterval(timer)
 				}	
-				console.log(Date.now()/1000);
-				i++;
+				console.log(Date.now()/1000)
+				i++
 			}, 1000)
 		}
-	};
+	}
 
-	$: $accessToken, refreshTimer();
+	$: $accessToken, refreshTimer()
 </script>
 
 <main>
 	<Navbar/>
-	{#if $isSinglePost == true}
-		<Singlepost singlePost={singlePost}/>
-	{:else}
-		<svelte:component this={pageMap[$activePage]} on:message={loadSinglePost}/>
-	{/if}
+	<svelte:component this={pageMap[$activePage]}/>
 </main>
 
 <style global>
