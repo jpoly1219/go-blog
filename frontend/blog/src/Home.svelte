65140cea8098a238e8b994076f3bedb86a2b0ad0
Jacob Kim <jpoly1219@gmail.com>
2021-06-24 13:55:56 +0000

    Removed semicolons

--- frontend/blog/src/Home.svelte
+++ frontend/blog/src/Home.svelte
@@ -1,9 +1,9 @@
 <script>
-    import { onMount } from "svelte";
-    import Post from "./Post.svelte";
+    import { onMount } from "svelte"
+    import Post from "./Post.svelte"
     
-    const apiURL = "http://jpoly1219devbox.xyz:8090/posts";
-	let postList = [];
+    const apiURL = "http://jpoly1219devbox.xyz:8090/posts"
+	let postList = []
 
 	onMount(async () => {
 		const res = await fetch(apiURL)
