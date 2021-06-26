0d68da8f8ab8fd4abaa622edc8ed2baead849f65
Jacob Kim <jpoly1219@gmail.com>
2021-06-26 06:28:02 +0000

    Created Profile component
    - Profile holds user information such as full name, username, email and password.
    
    - There aren't any javascript code to make the page dynamic yet, but it will be worked on soon.

---
+++ frontend/blog/src/Profile.svelte
@@ -0,0 +1,55 @@
+<script>
+    let fullname = "Sam Boezwinkle"
+    let username = "sam"
+    let email = "sam@gmail.com"
+    let password = "qwe123!@#"
+</script>
+
+<div class="container mx-auto w-2/5 shadow">
+    <div class="bg-white rounded-t-lg">
+        <div class="px-7 py-5">
+            <h3 class="text-lg leading-6 font-medium text-gray-900">
+                User Profile
+            </h3>
+            <p class="mt-1 text-sm text-gray-500">
+                Edit your profile and account information here.
+            </p>
+        </div>
+    </div>
+    <div class="border-t border-gray-200">
+        <form action="submit">
+            <div class="bg-gray-50 px-7 py-5 flex flex-row justify-between">
+                <h4 class="text-base text-gray-500 font-medium mr-52 self-center">
+                    Full Name
+                </h4>
+                <input type="text" value={fullname} class="text-base py-2 flex-grow max-w-sm">
+            </div>
+            <div class="bg-white px-7 py-5 flex flex-row justify-between">
+                <h4 class="text-base text-gray-500 font-medium mr-52 self-center">
+                    Username
+                </h4>
+                <input type="text" value={username} class="text-base py-2 flex-grow max-w-sm">
+            </div>
+            <div class="bg-gray-50 px-7 py-5 flex flex-row justify-between">
+                <h4 class="text-base text-gray-500 font-medium mr-52 self-center">
+                    Email
+                </h4>
+                <input type="password" value={email} class="text-base py-2 flex-grow max-w-sm">
+            </div>
+            <div class="bg-white px-7 py-5 flex flex-row justify-between">
+                <h4 class="text-base text-gray-500 font-medium mr-52 self-center">
+                    Password
+                </h4>
+                <input type="text" value={password} class="text-base py-2 flex-grow max-w-sm">
+            </div>
+            <div class="bg-gray-50 px-7 py-5 flex flex-row justify-end">
+                <button type="submit" class="inline-flex items-center bg-blue-400 border border-blue-900 rounded-lg text-base">
+                    <span class="mx-3 my-2 text-white">Save changes</span>
+                </button>
+                <button type="button" class="inline-flex items-center ml-5 border border-gray-500 rounded-lg">
+                    <span class="mx-3 my-2">Return to home</span>
+                </button>
+            </div>
+        </form>
+    </div>
+</div>
