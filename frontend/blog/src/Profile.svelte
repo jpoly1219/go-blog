991ac15142c507aabeccc6699b06cc53194c69ff
Jacob Kim <jpoly1219@gmail.com>
2021-06-27 06:47:31 +0000

    Profile no longer has edit form, instead loads user data and user posts
    - Previously, Post had a form where users could edit their profile data. That feature has been moved to Editprofile.
    
    - Profile now calls the backend for user data and his/her posts.
    
    - User data is shown on a card that floats to the left of user posts.

--- frontend/blog/src/Profile.svelte
+++ frontend/blog/src/Profile.svelte
@@ -1,55 +1,49 @@
 <script>
-    let fullname = "Sam Boezwinkle"
-    let username = "sam"
-    let email = "sam@gmail.com"
-    let password = "qwe123!@#"
+    import { onMount } from "svelte"
+    import Post from "./Post.svelte"
+
+    let userId
+	let userData = {
+        name: "",
+        username: "",
+        email: ""
+    }
+    let postData = []
+
+	onMount(async () => {
+		const res = await fetch("http://jpoly1219devbox.xyz:8090/sam123")
+		userData = await res.json()
+        userId = userData.id.toString()
+        
+        const res2 = await fetch("http://jpoly1219devbox.xyz:8090/"+userId+"/posts")
+        postData = await res2.json()
+	})
 </script>
 
-<div class="container mx-auto w-2/5 shadow">
-    <div class="bg-white rounded-t-lg">
-        <div class="px-7 py-5">
-            <h3 class="text-lg leading-6 font-medium text-gray-900">
-                User Profile
-            </h3>
-            <p class="mt-1 text-sm text-gray-500">
-                Edit your profile and account information here.
-            </p>
+<div class="flex flex-row static">
+    <div class="fixed w-1/3">
+        <div class="absolute top-0 right-10 w-1/2 mx-auto bg-white rounded-lg overflow-hidden shadow">
+            <div class="bg-cover h-40" style="background-image: url(https://free4kwallpapers.com/uploads/originals/2021/04/14/minimal-pacman-wallpaper.jpg)"></div>
+            <div class="border-b px-4 pb-4">
+                <div class="flex flex-col text-center">
+                    <img class="place-self-center h-32 w-32 rounded-full border-white border-4 -mt-16" src="https://widgetwhats.com/app/uploads/2019/11/free-profile-photo-whatsapp-4-300x300.png" alt>
+                    <div class="pt-2 pb-4">
+                        <h3 class="font-bold text-2x1 mb-1">{userData.name}</h3>
+                        <p class="text-gray-500">{userData.email}</p>
+                        <p class="text-gray-500">{userData.username}</p>
+                    </div>
+                    <button type="button" class="place-self-center bg-blue-400 text-white text-base rounded-lg px-3 py-2">
+                        <span>Follow</span>
+                    </button>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="border-t border-gray-200">
-        <form action="submit">
-            <div class="bg-gray-50 px-7 py-5 flex flex-row justify-between">
-                <h4 class="text-base text-gray-500 font-medium mr-52 self-center">
-                    Full Name
-                </h4>
-                <input type="text" value={fullname} class="text-base py-2 flex-grow max-w-sm">
-            </div>
-            <div class="bg-white px-7 py-5 flex flex-row justify-between">
-                <h4 class="text-base text-gray-500 font-medium mr-52 self-center">
-                    Username
-                </h4>
-                <input type="text" value={username} class="text-base py-2 flex-grow max-w-sm">
-            </div>
-            <div class="bg-gray-50 px-7 py-5 flex flex-row justify-between">
-                <h4 class="text-base text-gray-500 font-medium mr-52 self-center">
-                    Email
-                </h4>
-                <input type="password" value={email} class="text-base py-2 flex-grow max-w-sm">
-            </div>
-            <div class="bg-white px-7 py-5 flex flex-row justify-between">
-                <h4 class="text-base text-gray-500 font-medium mr-52 self-center">
-                    Password
-                </h4>
-                <input type="text" value={password} class="text-base py-2 flex-grow max-w-sm">
-            </div>
-            <div class="bg-gray-50 px-7 py-5 flex flex-row justify-end">
-                <button type="submit" class="inline-flex items-center bg-blue-400 border border-blue-900 rounded-lg text-base">
-                    <span class="mx-3 my-2 text-white">Save changes</span>
-                </button>
-                <button type="button" class="inline-flex items-center ml-5 border border-gray-500 rounded-lg">
-                    <span class="mx-3 my-2">Return to home</span>
-                </button>
-            </div>
-        </form>
+    <div class="w-1/3"></div>
+    <div class="container mx-auto w-1/3 flex flex-col">
+        {#each postData as post}
+            <Post post={post}/>
+        {/each}
     </div>
+    <div class="w-1/3"></div>
 </div>
