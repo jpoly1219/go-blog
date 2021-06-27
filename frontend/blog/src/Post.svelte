a6442d96cb9c084dbdd1ebcfe5a1ebe15be7ce01
Jacob Kim <jpoly1219@gmail.com>
2021-06-27 06:46:15 +0000

    Moved the outermost div tag to Home
    - It just makes more sense for Post to only handle how each individual post looks like, rather than the div that surrounds it.

--- frontend/blog/src/Post.svelte
+++ frontend/blog/src/Post.svelte
@@ -8,16 +8,14 @@
     }
 </script>
 
-<div class="container mx-auto px-96 pb-10 flex flex-col">
-    <div class="border-b-2">
-        <h2 on:click={setPostId} class="text-2xl font-medium text-gray-900 mt-4 mb-4 cursor-pointer">
-            {post.title}
-        </h2>
-        <h3 class="font-medium text-gray-900">
-            {post.author}
-        </h3>
-        <p class="leading-relaxed mb-8">
-            {post.content}
-        </p>
-    </div>
+<div class="border-b-2">
+    <h2 on:click={setPostId} class="text-2xl font-medium text-gray-900 mt-4 mb-4 cursor-pointer">
+        {post.title}
+    </h2>
+    <h3 class="font-medium text-gray-900">
+        {post.author}
+    </h3>
+    <p class="leading-relaxed mb-8">
+        {post.content}
+    </p>
 </div>
