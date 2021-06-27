7198d6c9bcc3cacbe176d26830fc45b36bf53383
Jacob Kim <jpoly1219@gmail.com>
2021-06-27 06:42:13 +0000

    Created OtherUser model
    - OtherUser is basically a User model, but without the Password field. This is useful for sending back other user's data.

--- models/models.go
+++ models/models.go
@@ -21,11 +21,18 @@ type User struct {
 	Password string `json:"password"`
 }
 
+type OtherUser struct {
+	Id       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Username string `json:"username"`
+}
+
 type Token struct {
 	AccessToken   string `json:"accesstoken"`
 	RefreshToken  string `json:"refreshtoken"`
 	AccessUuid    string `json:"accessuuid"`
 	RefreshUuid   string `json:"refreshuuid"`
-	AccessExpire  int64 `json:"accessexpire"`
-	RefreshExpire int64 `json:"refreshexpire"`
+	AccessExpire  int64  `json:"accessexpire"`
+	RefreshExpire int64  `json:"refreshexpire"`
 }
