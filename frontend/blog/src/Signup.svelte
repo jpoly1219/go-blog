223377c2b6092c430bded49ae65074ef1b544894
Jacob Kim <jpoly1219@gmail.com>
2021-06-30 11:01:06 +0000

    signin() is replaced by submit(), input values are bound to the variables defined in the script
    - signin() was a placeholder that just alerted the user that the event handler was working.
    
    - submit() is an async function that gets the name, email, username and password values from <input> tags, then POSTing them to the backend via /auth/signup endpoint.

--- frontend/blog/src/Signup.svelte
+++ frontend/blog/src/Signup.svelte
@@ -1,8 +1,29 @@
 <script>
     import { activePage } from "./stores"
 
-    function signin() {
-        alert("now signed up!")
+    let name = ""
+    let email = ""
+    let username = ""
+    let password = ""
+
+    async function submit() {
+        let loginDetails = {
+            name: name,
+            email: email,
+            username: username,
+            password: password
+        }
+
+        const options = {
+            method: "POST",
+            body: JSON.stringify(loginDetails),
+            headers: {
+                "Content-Type": "application/json"
+            },
+            credentials: "include"
+        }
+        const res = await fetch("http://jpoly1219devbox.xyz:8090/auth/signup", options)
+        alert("Now signed in! Please log in.")
         activePage.set("login")
     }
 </script>
@@ -10,15 +31,15 @@
 <div class="container mx-auto items-center flex flex-col text-gray-900">
     <div class="bg-white p-16 rounded-lg border-gray-900 shadow-lg my-20">
         <h2 class="text-2xl font-medium text-center mb-20">Join Goblog.</h2>
-        <form action="/" class="flex flex-col">
+        <form on:submit|preventDefault={submit} class="flex flex-col">
             <div class="flex flex-col items-stretch">
-                <input type="text" placeholder="Name" class="border-b-2 py-4">
-                <input type="text" placeholder="Email address" class="border-b-2 py-4">
-                <input type="text" placeholder="Username" class="border-b-2 py-4">
-                <input type="text" placeholder="Password" class="border-b-2 py-4">
+                <input bind:value={name} type="text" placeholder="Name" class="border-b-2 py-4">
+                <input bind:value={email} type="text" placeholder="Email address" class="border-b-2 py-4">
+                <input bind:value={username} type="text" placeholder="Username" class="border-b-2 py-4">
+                <input bind:value={password} type="password" placeholder="Password" class="border-b-2 py-4">
             </div>
             <div class="flex flex-col items-center my-10">
-                <button type="submit" on:click={signin} class="border bg-blue-400 rounded-lg p-3">
+                <button type="submit" class="border bg-blue-400 rounded-lg p-3">
                     <span class="mx-3 my-2 text-white">Sign up</span>
                 </button>
             </div>
