da9c2c7c263437004511ab1baff46258e2b4a3aa
Jacob Kim <jpoly1219@gmail.com>
2021-07-05 11:44:10 +0000

    Users can now view the author's profile when clicking on them; posts longer than 100 words are truncated
    - Clicking on the author's username will run goToProfile(), which sets the viewUser store to the username of the post's author, then redirects the user to the Profile component.
    
    - Using formatContent, each post content is formatted so that it would be truncated after the 100th word, then be followed by elipses.

--- frontend/blog/src/Post.svelte
+++ frontend/blog/src/Post.svelte
@@ -1,21 +1,46 @@
 <script>
-    import { activePage, postId } from "./stores"
+import { onMount } from "svelte";
+
+    import { activePage, postId, viewUser } from "./stores"
     export let post
     
     function setPostId() {
         postId.set(post.id)
         activePage.set("singlepost")
     }
+
+    function goToProfile() {
+        viewUser.set(post.author)
+        activePage.set("profile")
+    }
+
+    let originalContent = post.content
+    let wordArray = originalContent.split(" ")
+
+    function formatContent(wordArray) {
+        wordArray = wordArray.slice(0, 50)
+        const formattedContent = wordArray.join(" ").concat("...")
+        return formattedContent
+    }
+
+    let newContent
+    if (wordArray.length > 50) {
+        newContent = formatContent(wordArray)
+    }
+    else {
+        newContent = post.content
+    }
+
 </script>
 
 <div class="border-b-2">
     <h2 on:click={setPostId} class="text-2xl font-medium text-gray-900 mt-4 mb-4 cursor-pointer">
         {post.title}
     </h2>
-    <h3 class="font-medium text-gray-900">
+    <h3 on:click={goToProfile} class="font-medium text-gray-900 cursor-pointer">
         {post.author}
     </h3>
     <p class="leading-relaxed mb-8">
-        {post.content}
+        {newContent}
     </p>
 </div>
