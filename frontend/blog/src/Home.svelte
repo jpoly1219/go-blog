176f5093634bf8543de0a637c676cb0c4fb9b01a
Jacob Kim <jpoly1219@gmail.com>
2021-07-08 14:42:01 +0000

    Imported pagination from stores.js; created new div to show loading status

--- frontend/blog/src/Home.svelte
+++ frontend/blog/src/Home.svelte
@@ -1,5 +1,6 @@
 <script>
     import { onMount } from "svelte"
+	import { pagination } from "./stores.js"
     import Post from "./Post.svelte"
     
     const apiURL = "http://jpoly1219devbox.xyz:8090/posts"
@@ -16,4 +17,12 @@
 	{#each postList as post}
     	<Post post={post} on:message/>
 	{/each}
+	{#if postList != []}
+	<div class="text-center my-48">
+		<span class="text-2xl font-medium text-gray-500">Loading new posts...</span>
+	</div>
+	{/if}
 </div>
+
+<!-- https://svelte.dev/repl/4b8ccdf1d01545baa0ab6a858bc05abb?version=3.32.1 -->
+<!-- https://levelup.gitconnected.com/loading-more-results-on-scroll-with-svelte-js-restful-apis-svelte-infinite-scrolling-ad80a09b5e33 -->
